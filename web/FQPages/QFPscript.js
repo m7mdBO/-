139326a0540bd2ba3d712c37578e2273187b8893
m7mdBO <mohammedmxd8@gmail.com>
2024-06-21 07:33:32 +0300

    flags

---
+++ web/FQPages/QFPscript.js
@@ -0,0 +1,14 @@
+// QFPscript.js
+document.addEventListener('DOMContentLoaded', () => {
+    const card = document.querySelector('.card1');
+    const svgHolder = document.querySelector('.svg-holder');
+    const svgHolderBack = document.querySelector('.svg-holder-back');
+
+    svgHolder.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+
+    svgHolderBack.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+});
