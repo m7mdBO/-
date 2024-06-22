25487034a6f5fa49a9c20f7f44681fe163bbfae6
m7mdBO <mohammedmxd8@gmail.com>
2024-06-22 22:56:07 +0300

    400

--- web/FQPages/p.js
+++ web/FQPages/p.js
@@ -63,10 +63,10 @@ document.addEventListener('DOMContentLoaded', () => {
         let team2Score = parseInt(localStorage.getItem('team2Score') || '0');
 
         if (team === 'team1') {
-            team1Score += 200;
+            team1Score += 400;
             localStorage.setItem('team1Score', team1Score.toString());
         } else if (team === 'team2') {
-            team2Score += 200;
+            team2Score += 400;
             localStorage.setItem('team2Score', team2Score.toString());
         }
 
@@ -120,4 +120,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add more disableLink calls for other links as needed
 });
-
